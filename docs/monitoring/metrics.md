1214cf3ab15756fa393b437bd18abe47ba6a3f23
David Brinegar <david@opsclarity.com>
2017-06-26 16:13:46 -0700

    [FLINK-7009] dogstatsd mode in statds reporter
    * converts output to ascii alphanumeric characters with underbar,
    delimited by periods
    * reports all Flink variables as tags
    * compresses overly long segments with a first-ten plus hash symbol
    * compresses Flink ID values to first eight characters
    * removes object references from names, for correctness
    * drops negative and invalid values
    * handles LatencyGauge values

--- docs/monitoring/metrics.md
+++ docs/monitoring/metrics.md
@@ -451,10 +451,32 @@ All Flink metrics variables (see [List of all Variables](#list-of-all-variables)
 In order to use this reporter you must copy `/opt/flink-metrics-statsd-{{site.version}}.jar` into the `/lib` folder
 of your Flink distribution.
 
+In `dogstatsd` mode, all variables in Flink metrics such as `<host>`, `<job_name>`, `<tm_id>`, `<subtask_index>`, `<task_name>`,
+`<operator_name>` and others, will be included as tags.  The domain always begins with `flink` followed by a generalized metric
+identifier. In contrast to the usual identifier it is not affected by scope-formats, does not contain any variables and is
+constant across jobs. An example for such a domain would be `flink.job.task.numBytesOut`.
+
+
+In `shortids` mode, Flink variables like `<tm_id>`, `<task_id>`, `<job_id>`, `<task_attempt_id>` will print only the first
+8 hex characters of the 32 hex character value.  This is to provide good enough distinction and identification, while
+avoiding truncation in the case where metrics are otherwise too long.
+
+Statsd metrics are output with ascii alphanumeric characters, separated by underbar and period characters, and attempted
+to be kept short enough to survive without truncation.  Instance references like serializer object hashes are removed from
+names, where found, so metric names are stable across runs. Where task or operator names are quite long, such as can be
+seen with latency metrics or default trigger window names, the name will be compressed to the first ten valid characters
+and a short hash signature of the original name.  For example, you might see `TriggerWin_c2910b88` instead of the default
+Trigger Window name.  Readable names can be provided by your application where needed, this compression strategy is more for
+dealing with long and complex default names.
+
+This reporter handles latency metrics, in addition to the usual types.
+
 Parameters:
 
 - `host` - the StatsD server host
 - `port` - the StatsD server port
+- `dogstatsd` - use dogstatsd tags, default false
+- `shortids` - use short ids, default false
 
 Example configuration:
 
@@ -466,6 +488,19 @@ metrics.reporter.stsd.host: localhost
 metrics.reporter.stsd.port: 8125
 
 {% endhighlight %}
+
+{% highlight yaml %}
+
+// in dogstatsd mode, all scope variables will be provided in tags:
+metrics.reporters: stsd
+metrics.reporter.stsd.class: org.apache.flink.metrics.statsd.StatsDReporter
+metrics.reporter.stsd.host: localhost
+metrics.reporter.stsd.port: 8125
+metrics.reporter.stsd.dogstatsd: true
+metrics.reporter.stsd.shortids: true
+
+{% highlight yaml %}
+
 
 ### Datadog (org.apache.flink.metrics.datadog.DatadogHttpReporter)
 
--- flink-metrics/flink-metrics-statsd/src/main/java/org/apache/flink/metrics/statsd/StatsDReporter.java
+++ flink-metrics/flink-metrics-statsd/src/main/java/org/apache/flink/metrics/statsd/StatsDReporter.java
@@ -24,9 +24,13 @@ import org.apache.flink.metrics.Gauge;
 import org.apache.flink.metrics.Histogram;
 import org.apache.flink.metrics.HistogramStatistics;
 import org.apache.flink.metrics.Meter;
+import org.apache.flink.metrics.Metric;
 import org.apache.flink.metrics.MetricConfig;
-import org.apache.flink.metrics.reporter.AbstractReporter;
+import org.apache.flink.metrics.MetricGroup;
+import org.apache.flink.metrics.reporter.AbstractReporterV2;
 import org.apache.flink.metrics.reporter.Scheduled;
+import org.apache.flink.runtime.metrics.groups.AbstractMetricGroup;
+import org.apache.flink.runtime.metrics.groups.FrontMetricGroup;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -40,31 +44,62 @@ import java.nio.charset.StandardCharsets;
 import java.util.ConcurrentModificationException;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * Largely based on the StatsDReporter class by ReadyTalk.
+ * Originally based on the StatsDReporter class by ReadyTalk.
  *
  * <p>https://github.com/ReadyTalk/metrics-statsd/blob/master/metrics3-statsd/src/main/java/com/readytalk/metrics/StatsDReporter.java
  *
  * <p>Ported since it was not present in maven central.
  */
 @PublicEvolving
-public class StatsDReporter extends AbstractReporter implements Scheduled {
+public class StatsDReporter extends AbstractReporterV2<StatsDReporter.TaggedMetric> implements Scheduled {
+
+	class TaggedMetric {
+		private final String name;
+		private final String tags;
+		TaggedMetric(String name, String tags) {
+			this.name = name;
+			this.tags = tags;
+		}
+
+		String getName() {
+			return name;
+		}
+
+		String getTags() {
+			return tags;
+		}
+	}
 
 	private static final Logger LOG = LoggerFactory.getLogger(StatsDReporter.class);
 
-	public static final String ARG_HOST = "host";
-	public static final String ARG_PORT = "port";
+	private static final String ARG_HOST = "host";
+	private static final String ARG_PORT = "port";
+	private static final String ARG_DOGSTATSD = "dogstatsd";
+	private static final String ARG_SHORTIDS = "shortids";
 
 	private boolean closed = false;
 
 	private DatagramSocket socket;
 	private InetSocketAddress address;
 
+	private boolean dogstatsdMode;
+	private boolean shortIds;
+
+	private final Pattern instanceRef = Pattern.compile("@[a-f0-9]+");
+	private final Pattern flinkId = Pattern.compile("[a-f0-9]{32}");
+	private static final char SCOPE_SEPARATOR = '.';
+
 	@Override
 	public void open(MetricConfig config) {
 		String host = config.getString(ARG_HOST, null);
 		int port = config.getInteger(ARG_PORT, -1);
+
+		dogstatsdMode = config.getBoolean(ARG_DOGSTATSD, false);
+		shortIds = config.getBoolean(ARG_SHORTIDS, false);
 
 		if (host == null || host.length() == 0 || port < 1) {
 			throw new IllegalArgumentException("Invalid host/port configuration. Host: " + host + " Port: " + port);
@@ -77,7 +112,7 @@ public class StatsDReporter extends AbstractReporter implements Scheduled {
 		} catch (SocketException e) {
 			throw new RuntimeException("Could not create datagram socket. ", e);
 		}
-		log.info("Configured StatsDReporter with {host:{}, port:{}}", host, port);
+		log.info("Configured StatsDReporter with config: {}", config);
 	}
 
 	@Override
@@ -90,31 +125,70 @@ public class StatsDReporter extends AbstractReporter implements Scheduled {
 
 	// ------------------------------------------------------------------------
 
+	/**
+	 * Removes leading and trailing angle brackets.
+	 */
+	private String stripBrackets(String str) {
+		return str.substring(1, str.length() - 1);
+	}
+
+	@SuppressWarnings("unchecked")
+	private String getLogicalScope(MetricGroup group) {
+		return ((FrontMetricGroup<AbstractMetricGroup<?>>) group).getLogicalScope(this, SCOPE_SEPARATOR);
+	}
+
+	@Override
+	protected TaggedMetric getMetricInfo(Metric metric, String metricName, MetricGroup group) {
+		if (dogstatsdMode) {
+			String name = "flink" + SCOPE_SEPARATOR + getLogicalScope(group) + SCOPE_SEPARATOR + filterCharacters(metricName);
+
+			// memoize dogstatsd tag section: "|#tag:val,tag:val,tag:val"
+			StringBuilder statsdTagLine = new StringBuilder();
+			for (Map.Entry<String, String> entry: group.getAllVariables().entrySet()) {
+				String k = stripBrackets(entry.getKey());
+				String v = filterCharacters(entry.getValue());
+				if (statsdTagLine.length() == 0) {
+					// begin tag section with dogstatsd format |#
+					statsdTagLine.append("|#");
+				} else {
+					// separate multiple k:v with commas
+					statsdTagLine.append(',');
+				}
+				statsdTagLine.append(k).append(':').append(v);
+			}
+			return new TaggedMetric(name, statsdTagLine.toString());
+		} else {
+			// the simple case with one scoped name
+			String name = group.getMetricIdentifier(metricName, this);
+			return new TaggedMetric(name, null);
+		}
+	}
+
 	@Override
 	public void report() {
 		// instead of locking here, we tolerate exceptions
 		// we do this to prevent holding the lock for very long and blocking
 		// operator creation and shutdown
 		try {
-			for (Map.Entry<Gauge<?>, String> entry : gauges.entrySet()) {
+			for (Map.Entry<Gauge<?>, TaggedMetric> entry : gauges.entrySet()) {
 				if (closed) {
 					return;
 				}
 				reportGauge(entry.getValue(), entry.getKey());
 			}
 
-			for (Map.Entry<Counter, String> entry : counters.entrySet()) {
+			for (Map.Entry<Counter, TaggedMetric> entry : counters.entrySet()) {
 				if (closed) {
 					return;
 				}
 				reportCounter(entry.getValue(), entry.getKey());
 			}
 
-			for (Map.Entry<Histogram, String> entry : histograms.entrySet()) {
+			for (Map.Entry<Histogram, TaggedMetric> entry : histograms.entrySet()) {
 				reportHistogram(entry.getValue(), entry.getKey());
 			}
 
-			for (Map.Entry<Meter, String> entry : meters.entrySet()) {
+			for (Map.Entry<Meter, TaggedMetric> entry : meters.entrySet()) {
 				reportMeter(entry.getValue(), entry.getKey());
 			}
 		}
@@ -126,42 +200,56 @@ public class StatsDReporter extends AbstractReporter implements Scheduled {
 
 	// ------------------------------------------------------------------------
 
-	private void reportCounter(final String name, final Counter counter) {
-		send(name, String.valueOf(counter.getCount()));
+	private void reportCounter(final TaggedMetric metric, final Counter counter) {
+		send(metric.getName(), String.valueOf(counter.getCount()), metric.getTags());
 	}
 
-	private void reportGauge(final String name, final Gauge<?> gauge) {
+	private void reportGauge(final TaggedMetric metric, final Gauge<?> gauge) {
 		Object value = gauge.getValue();
-		if (value != null) {
-			send(name, value.toString());
+		if (value == null) {
+			return;
+		}
+		if (value instanceof Map && gauge.getClass().getSimpleName().equals("LatencyGauge")) {
+			// LatencyGauge is a Map<String, HashMap<String,Double>>
+			for (Object m: ((Map<?, ?>) value).values()) {
+				if (m instanceof Map) {
+					for (Map.Entry<?, ?> entry: ((Map<?, ?>) m).entrySet()) {
+						String k = String.valueOf(entry.getKey());
+						String v = String.valueOf(entry.getValue());
+						send(prefix(metric.getName(), k), v, metric.getTags());
+					}
+				}
+			}
+		} else {
+			send(metric.getName(), value.toString(), metric.getTags());
 		}
 	}
 
-	private void reportHistogram(final String name, final Histogram histogram) {
+	private void reportHistogram(final TaggedMetric metric, final Histogram histogram) {
 		if (histogram != null) {
 
 			HistogramStatistics statistics = histogram.getStatistics();
 
 			if (statistics != null) {
-				send(prefix(name, "count"), String.valueOf(histogram.getCount()));
-				send(prefix(name, "max"), String.valueOf(statistics.getMax()));
-				send(prefix(name, "min"), String.valueOf(statistics.getMin()));
-				send(prefix(name, "mean"), String.valueOf(statistics.getMean()));
-				send(prefix(name, "stddev"), String.valueOf(statistics.getStdDev()));
-				send(prefix(name, "p50"), String.valueOf(statistics.getQuantile(0.5)));
-				send(prefix(name, "p75"), String.valueOf(statistics.getQuantile(0.75)));
-				send(prefix(name, "p95"), String.valueOf(statistics.getQuantile(0.95)));
-				send(prefix(name, "p98"), String.valueOf(statistics.getQuantile(0.98)));
-				send(prefix(name, "p99"), String.valueOf(statistics.getQuantile(0.99)));
-				send(prefix(name, "p999"), String.valueOf(statistics.getQuantile(0.999)));
+				send(prefix(metric.getName(), "count"), String.valueOf(histogram.getCount()), metric.getTags());
+				send(prefix(metric.getName(), "max"), String.valueOf(statistics.getMax()), metric.getTags());
+				send(prefix(metric.getName(), "min"), String.valueOf(statistics.getMin()), metric.getTags());
+				send(prefix(metric.getName(), "mean"), String.valueOf(statistics.getMean()), metric.getTags());
+				send(prefix(metric.getName(), "stddev"), String.valueOf(statistics.getStdDev()), metric.getTags());
+				send(prefix(metric.getName(), "p50"), String.valueOf(statistics.getQuantile(0.5)), metric.getTags());
+				send(prefix(metric.getName(), "p75"), String.valueOf(statistics.getQuantile(0.75)), metric.getTags());
+				send(prefix(metric.getName(), "p95"), String.valueOf(statistics.getQuantile(0.95)), metric.getTags());
+				send(prefix(metric.getName(), "p98"), String.valueOf(statistics.getQuantile(0.98)), metric.getTags());
+				send(prefix(metric.getName(), "p99"), String.valueOf(statistics.getQuantile(0.99)), metric.getTags());
+				send(prefix(metric.getName(), "p999"), String.valueOf(statistics.getQuantile(0.999)), metric.getTags());
 			}
 		}
 	}
 
-	private void reportMeter(final String name, final Meter meter) {
+	private void reportMeter(final TaggedMetric metric, final Meter meter) {
 		if (meter != null) {
-			send(prefix(name, "rate"), String.valueOf(meter.getRate()));
-			send(prefix(name, "count"), String.valueOf(meter.getCount()));
+			send(prefix(metric.getName(), "rate"), String.valueOf(meter.getRate()), metric.getTags());
+			send(prefix(metric.getName(), "count"), String.valueOf(meter.getCount()), metric.getTags());
 		}
 	}
 
@@ -170,7 +258,7 @@ public class StatsDReporter extends AbstractReporter implements Scheduled {
 			StringBuilder stringBuilder = new StringBuilder(names[0]);
 
 			for (int i = 1; i < names.length; i++) {
-				stringBuilder.append('.').append(names[i]);
+				stringBuilder.append(SCOPE_SEPARATOR).append(names[i]);
 			}
 
 			return stringBuilder.toString();
@@ -179,41 +267,130 @@ public class StatsDReporter extends AbstractReporter implements Scheduled {
 		}
 	}
 
-	private void send(final String name, final String value) {
+	private String buildStatsdLine(final String name, final String value, final String tags) {
+		Double number;
 		try {
-			String formatted = String.format("%s:%s|g", name, value);
-			byte[] data = formatted.getBytes(StandardCharsets.UTF_8);
-			socket.send(new DatagramPacket(data, data.length, this.address));
+			number = Double.parseDouble(value);
 		}
-		catch (IOException e) {
-			LOG.error("unable to send packet to statsd at '{}:{}'", address.getHostName(), address.getPort());
+		catch (NumberFormatException e) {
+			// quietly skip values like "n/a"
+			return "";
+		}
+		if (number >= 0.) {
+			return String.format("%s:%s|g%s", name, value, tags != null ? tags : "");
+		} else {
+			// quietly skip "unknowns" like lowWaterMark:-9223372036854775808, or JVM.Memory.NonHeap.Max:-1, or NaN
+			return "";
 		}
 	}
 
-	@Override
-	public String filterCharacters(String input) {
+	private void send(final String name, final String value, final String tags) {
+		String formatted = buildStatsdLine(name, value, tags);
+		if (formatted.length() > 0) {
+			try {
+				byte[] data = formatted.getBytes(StandardCharsets.UTF_8);
+				socket.send(new DatagramPacket(data, data.length, this.address));
+			}
+			catch (IOException e) {
+				LOG.error("unable to send packet to statsd at '{}:{}'", address.getHostName(), address.getPort());
+			}
+		}
+	}
+
+	/**
+	* dogstatsd names should: start with letter, uses ascii alphanumerics and underscore, separated by periods.
+	* Collapse runs of invalid characters into an underscore. Discard invalid prefix and suffix.
+	* Eg: ":::metric:::name:::" ->  "metric_name"
+	*/
+
+	private boolean isValidStatsdChar(char c) {
+		return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9') || (c == '_');
+	}
+
+	private String filterNCharacters(String input, int limit) {
 		char[] chars = null;
 		final int strLen = input.length();
 		int pos = 0;
+		boolean insertFiller = false;
 
-		for (int i = 0; i < strLen; i++) {
+		for (int i = 0; i < strLen && pos < limit; i++) {
 			final char c = input.charAt(i);
-			switch (c) {
-				case ':':
-					if (chars == null) {
-						chars = input.toCharArray();
+			if (isValidStatsdChar(c)) {
+				if (chars != null) {
+					// skip invalid suffix, only fill if followed by valid character
+					if (insertFiller) {
+						chars[pos++] = '_';
+						insertFiller = false;
 					}
-					chars[pos++] = '-';
-					break;
-
-				default:
-					if (chars != null) {
-						chars[pos] = c;
-					}
-					pos++;
+					chars[pos] = c;
+				}
+				pos++;
+			} else {
+				if (chars == null) {
+					chars = input.toCharArray();
+				}
+				// skip invalid prefix, until pos > 0
+				if (pos > 0) {
+					// collapse sequence of invalid char into one filler
+					insertFiller = true;
+				}
 			}
 		}
 
-		return chars == null ? input : new String(chars, 0, pos);
+		if (chars == null) {
+			if (strLen > limit) {
+				return input.substring(0, limit);
+			} else {
+				return input; // happy path, input is entirely valid and under the limit
+			}
+		} else {
+			return new String(chars, 0, pos);
+		}
+	}
+
+	/**
+	 * filterCharacters() is called on each delimited segment of the metric.
+	 *
+	 * <p>We might get a string that has coded structures, references to instances of serializers and reducers, and even if
+	 * we normalize all the odd characters could be overly long for a metric name, likely to be truncated downstream.
+	 * Our choices here appear to be either to discard invalid metrics, or to pragmatically handle each of the various
+	 * issues and produce something that might be useful in aggregate even though the named parts are hard to read.
+	 *
+	 * <p>This function will find and remove all object references like @abcd0123, so that task and operator names are stable.
+	 * The name of an operator should be the same every time it is run, so we should ignore object hash ids like these.
+	 *
+	 * <p>If the segment is a tm_id, task_id, job_id, task_attempt_id, we can optionally trim those to the first 8 chars.
+	 * This can reduce overall length substantially while still preserving enough to distinguish metrics from each other.
+	 *
+	 * <p>If the segment is 50 chars or longer, we will compress it to avoid truncation. The compression will look like the
+	 * first 10 valid chars followed by a hash of the original.  This sacrifices readability for utility as a metric, so
+	 * that latency metrics might survive with valid and useful dimensions for aggregation, even if it is very hard to
+	 * reverse engineer the particular operator name.  Application developers can of course still supply their own names
+	 * and are not forced to rely on the defaults.
+	 *
+	 * <p>This will turn something like:
+	 * 		"TriggerWindow(TumblingProcessingTimeWindows(5000), ReducingStateDescriptor{serializer=org.apache.flink.api.java
+	 * 		.typeutils.runtime.PojoSerializer@f3395ffa, reduceFunction=org.apache.flink.streaming.examples.socket.
+	 * 		SocketWindowWordCount$1@4201c465}, ProcessingTimeTrigger(), WindowedStream.reduce(WindowedStream.java-301))"
+	 *
+	 * <p>into:  "TriggerWin_c2910b88"
+	 */
+	@Override
+	public String filterCharacters(String input) {
+		// remove instance references
+		Matcher hasRefs = instanceRef.matcher(input);
+		if (hasRefs.find()) {
+			input = hasRefs.replaceAll("");
+		}
+		// compress segments too long
+		if (input.length() >= 50) {
+			return filterNCharacters(input, 10) + "_" + Integer.toHexString(input.hashCode());
+		}
+		int limit = Integer.MAX_VALUE;
+		// optionally shrink flink ids
+		if (shortIds && input.length() == 32 && flinkId.matcher(input).matches()) {
+			limit = 8;
+		}
+		return filterNCharacters(input, limit);
 	}
 }
