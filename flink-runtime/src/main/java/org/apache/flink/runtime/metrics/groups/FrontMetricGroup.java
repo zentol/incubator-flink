apache/flink

[FLINK-14350][metrics] Introduce dedicated MetricScope

flink-runtime/src/main/java/org/apache/flink/runtime/metrics/groups/FrontMetricGroup.java
@@ -19,6 +19,10 @@
 package org.apache.flink.runtime.metrics.groups;
 
 import org.apache.flink.metrics.CharacterFilter;
+import org.apache.flink.metrics.MetricScope;
+import org.apache.flink.runtime.metrics.scope.InternalMetricScope;
+
+import java.util.Map;
 
 /**
  * Metric group which forwards all registration calls to a variable parent metric group that injects a variable reporter
@@ -30,21 +34,21 @@ import org.apache.flink.metrics.CharacterFilter;
  */
 public class FrontMetricGroup<P extends AbstractMetricGroup<?>> extends ProxyMetricGroup<P> {
 
-	protected int reporterIndex;
+	private final ReporterIndexInjectingMetricScope scope;
 
 	public FrontMetricGroup(int reporterIndex, P reference) {
 		super(reference);
-		this.reporterIndex = reporterIndex;
+		this.scope = new ReporterIndexInjectingMetricScope(reporterIndex, this.parentMetricGroup.getScope());
 	}
 
 	@Override
 	public String getMetricIdentifier(String metricName) {
-		return parentMetricGroup.getMetricIdentifier(metricName, null, this.reporterIndex);
+		return scope.getMetricIdentifier(metricName);
 	}
 
 	@Override
 	public String getMetricIdentifier(String metricName, CharacterFilter filter) {
-		return parentMetricGroup.getMetricIdentifier(metricName, filter, this.reporterIndex);
+		return scope.getMetricIdentifier(metricName, filter);
 	}
 
 	public String getLogicalScope(CharacterFilter filter) {
@@ -52,6 +56,32 @@ public class FrontMetricGroup<P extends AbstractMetricGroup<?>> extends ProxyMet
 	}
 
 	public String getLogicalScope(CharacterFilter filter, char delimiter) {
-		return parentMetricGroup.getLogicalScope(filter, delimiter, this.reporterIndex);
+		return parentMetricGroup.getLogicalScope(filter, delimiter);
+	}
+
+	private static final class ReporterIndexInjectingMetricScope implements MetricScope {
+
+		private final int reporterIndex;
+		private final InternalMetricScope scope;
+
+		private ReporterIndexInjectingMetricScope(int reporterIndex, InternalMetricScope scope) {
+			this.reporterIndex = reporterIndex;
+			this.scope = scope;
+		}
+
+		@Override
+		public Map<String, String> getAllVariables() {
+			return scope.getAllVariables();
+		}
+
+		@Override
+		public String getMetricIdentifier(String metricName) {
+			return scope.getMetricIdentifier(metricName, s -> s, reporterIndex);
+		}
+
+		@Override
+		public String getMetricIdentifier(String metricName, CharacterFilter filter) {
+			return scope.getMetricIdentifier(metricName, filter, reporterIndex);
+		}
 	}
 }

zentol/incubator-flink

flink-runtime/src/main/java/org/apache/flink/runtime/metrics/groups/FrontMetricGroup.java
@@ -19,6 +19,8 @@
 package org.apache.flink.runtime.metrics.groups;
 
 import org.apache.flink.metrics.CharacterFilter;
+import org.apache.flink.metrics.MetricScope;
+import org.apache.flink.runtime.metrics.scope.InternalMetricScope;
 
 import java.util.Map;
 
@@ -30,35 +32,70 @@ import java.util.Map;
  *
  * @param <P> parentMetricGroup to {@link AbstractMetricGroup AbstractMetricGroup}
  */
+@SuppressWarnings("deprecation")
 public class FrontMetricGroup<P extends AbstractMetricGroup<?>> extends ProxyMetricGroup<P> {
 
 	private final ReporterScopedSettings settings;
 
+	private final ReporterIndexInjectingMetricScope scope;
+
 	public FrontMetricGroup(ReporterScopedSettings settings, P reference) {
 		super(reference);
 		this.settings = settings;
+		this.scope = new ReporterIndexInjectingMetricScope(settings, this.parentMetricGroup.getScope());
 	}
 
 	@Override
 	public String getMetricIdentifier(String metricName) {
-		return parentMetricGroup.getMetricIdentifier(metricName, null, this.settings.getReporterIndex(), this.settings.getDelimiter());
+		return scope.getMetricIdentifier(metricName);
 	}
 
 	@Override
 	public String getMetricIdentifier(String metricName, CharacterFilter filter) {
-		return parentMetricGroup.getMetricIdentifier(metricName, filter, this.settings.getReporterIndex(), this.settings.getDelimiter());
+		return scope.getMetricIdentifier(metricName, filter);
 	}
 
 	@Override
 	public Map<String, String> getAllVariables() {
-		return parentMetricGroup.getAllVariables(this.settings.getReporterIndex(), this.settings.getExcludedVariables());
+		return scope.getAllVariables();
 	}
 
 	public String getLogicalScope(CharacterFilter filter) {
-		return parentMetricGroup.getLogicalScope(filter, this.settings.getDelimiter());
+		return parentMetricGroup.getLogicalScope(filter);
 	}
 
 	public String getLogicalScope(CharacterFilter filter, char delimiter) {
 		return parentMetricGroup.getLogicalScope(filter, delimiter, this.settings.getReporterIndex());
+	}
+
+	@Override
+	public MetricScope getScope() {
+		return scope;
+	}
+
+	private static final class ReporterIndexInjectingMetricScope implements MetricScope {
+
+		private final ReporterScopedSettings settings;
+		private final InternalMetricScope parentScope;
+
+		private ReporterIndexInjectingMetricScope(ReporterScopedSettings settings, InternalMetricScope parentScope) {
+			this.settings = settings;
+			this.parentScope = parentScope;
+		}
+
+		@Override
+		public Map<String, String> getAllVariables() {
+			return parentScope.getAllVariables();
+		}
+
+		@Override
+		public String getMetricIdentifier(String metricName) {
+			return parentScope.getMetricIdentifier(metricName, s -> s, settings.getReporterIndex());
+		}
+
+		@Override
+		public String getMetricIdentifier(String metricName, CharacterFilter filter) {
+			return parentScope.getMetricIdentifier(metricName, filter, settings.getReporterIndex());
+		}
 	}
 }